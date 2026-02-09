364312432691f41d73ed0d52b69c3c07c448d6e1
faijalfshaikh <faijalfshaikh@gmail.com>
2026-02-09 23:36:05 +0530

    Added Router to Application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Route,Routes} from "react-router-dom"
+import Home from './pages/Home'
+import ApplyJobs from './pages/ApplyJobs'
+import Applications from './pages/Applications'
+
 
 const App = () => {
   return (
-    <div>Job Portal</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/apply-job/:id' element={<ApplyJobs/>}></Route>
+        <Route path='/applications' element={<Applications/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
